7a7335c8f1698f36ac1c19029bd1b3b88ff19260
Ania Blanco <aniablanco2004@gmail.com>
2023-09-27 09:40:39 +0200

    Corregido de nuevo la tarea #3 y completada la #4:

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,10 +1,10 @@
-## Ejercicio 3
+## Ejercicio 3:
 
 def suma(numero1, numero2):
     return numero1+numero2
 
 def resta(numero1, numero2):
-    if(numero1<numero2):
+    if numero1<numero2:
         return numero2-numero1
     else:
         return numero1-numero2
@@ -13,9 +13,7 @@ def multiplicacion(numero1, numero2):
     return numero1*numero2
 
 def division(numero1,numero2):
-    if (numero1/0 or numero2/0):
-        print("Imposible realizar la operación, no se puede dividir entre 0. Da infinito")
-    elif(0/numero1 or 0/numero2):
-        print("El resultado será siempre = "+0)
+    if numero1==0 or numero2==0:
+        return print("Imposible realizar la operación, no se puede dividir entre 0. Da infinito")
     else:
-        numero1/numero2
+        return numero1/numero2
