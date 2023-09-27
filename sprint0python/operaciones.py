dfc811f2272486ed464d0cc1459c92def84ebb34
Ania Blanco <aniablanco2004@gmail.com>
2023-09-27 09:00:11 +0200

    Actualizada tarea #3.

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -13,7 +13,9 @@ def multiplicacion(numero1, numero2):
     return numero1*numero2
 
 def division(numero1,numero2):
-    if (numero1==0 or numero2==0):
-        print("Imposible realizar la operación, no se puede dividir entre 0.")
+    if (numero1/0 or numero2/0):
+        print("Imposible realizar la operación, no se puede dividir entre 0. Da infinito")
+    elif(0/numero1 or 0/numero2):
+        print("El resultado será siempre = "+0)
     else:
         numero1/numero2
