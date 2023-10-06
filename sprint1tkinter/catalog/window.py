9c64c6beaf74a5fdbc6b480f4fb89b74033b6cbb
Ania Blanco <aniablanco2004@gmail.com>
2023-10-06 11:55:27 +0200

    Completada tarea #6.

--- sprint1tkinter/catalog/window.py
+++ sprint1tkinter/catalog/window.py
@@ -3,27 +3,30 @@
 from tkinter import ttk, messagebox
 import tkinter as tk
 from cell import Cell
-from PIL import Image
-
+from PIL import Image, ImageTk
 
 class MainWindow():
 
     def on_button_clicked(self, cell):
-        message = "Has hecho click en el  juego: " + cell.title
+        message = "Has hecho click en el juego: " + cell.title
         messagebox.showinfo("Información", message)
 
     def __init__(self, root):
         root.title("Top 5 Juegos:")
 
         self.cells = [
-            Cell("Divinity Original Sin II", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\edited\\300px-Divinity_Original_Sin_II_-_Definitive_Edition_cover (100x100).jpg"),
-            Cell("Baldur's Gate 3", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\edited\\Baldurs gate 3 game cover (100x100).jpg"),
-            Cell("Mass Effect: Legendary Edition", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\edited\\Mass Effect game cover (100x100).jpg"),
-            Cell("Persona 5", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\edited\\Persona_5_cover_art (100x100).jpg"),
-            Cell("The Elder Scrolls V: Skyrim", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\edited\\The_Elder_Scrolls_V_Skyrim_cover (100x100).jpg")
+            Cell("Divinity Original Sin II", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\300px-Divinity_Original_Sin_II_-_Definitive_Edition_cover.jpg"),
+            Cell("Baldur's Gate 3", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\Baldurs gate 3 game cover.jpg"),
+            Cell("Mass Effect: Legendary Edition", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\Mass Effect game cover.jpg"),
+            Cell("Persona 5", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\Persona_5_cover_art.jpg"),
+            Cell("The Elder Scrolls V: Skyrim", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\The_Elder_Scrolls_V_Skyrim_cover.png")
         ]
 
+
         for i, cell in enumerate(self.cells):
-            label = ttk.Label(root, image = cell.image_tk, text = cell.title, compound = tk.BOTTOM)
-            label.grid(row = i, column = 0)
-            label.bind("<Button-1>", lambda event, cell = cell: self.on_button_clicked(cell))
+            juego = (Image.open(cell.path)).resize((100, 100), Image.Resampling.LANCZOS)
+            cell.image_tk = ImageTk.PhotoImage(juego)
+
+            label = ttk.Label(root, image=cell.image_tk, text=cell.title, compound= tk.BOTTOM)
+            label.grid(row=0,column=i)
+            label.bind("<Button-1>",lambda event, celda = cell: self.on_button_clicked(cell))
