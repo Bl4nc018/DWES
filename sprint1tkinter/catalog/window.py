8c2f66c364c927ed111522991fa69028805a4c0c
Ania Blanco <aniablanco2004@gmail.com>
2023-10-06 12:03:16 +0200

    Comentada tarea #6.

--- sprint1tkinter/catalog/window.py
+++ sprint1tkinter/catalog/window.py
@@ -8,11 +8,11 @@ from PIL import Image, ImageTk
 class MainWindow():
 
     def on_button_clicked(self, cell):
-        message = "Has hecho click en el juego: " + cell.title
+        message = "Has hecho click en el juego: " + cell.title ## Mensaje que aparecerá una vez se haga clic.
         messagebox.showinfo("Información", message)
 
     def __init__(self, root):
-        root.title("Top 5 Juegos:")
+        root.title("Top 5 Juegos:") ## Título de la ventana.
 
         self.cells = [
             Cell("Divinity Original Sin II", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\300px-Divinity_Original_Sin_II_-_Definitive_Edition_cover.jpg"),
@@ -22,11 +22,16 @@ class MainWindow():
             Cell("The Elder Scrolls V: Skyrim", "C:\\msys64\\home\\joker\\DWES\\sprint1tkinter\\catalog\\data\\unedited\\The_Elder_Scrolls_V_Skyrim_cover.png")
         ]
 
+        ## Celdas con el título que tendrán y, debajo, la imagen que mostrarán.
 
-        for i, cell in enumerate(self.cells):
+        for i, cell in enumerate(self.cells): ## Bucle para leer la lista.
+            
+            ## La siguiente línea se trata de una variable en la que se abre la ruta de la clase cell, para obtener la imagen. Mediante el uso de Image.open procede
+            ## a abrirla y, todo esto ejecuta .resize, lo que quiere decir que cambia la imagen que estaba sin editar a un 100x100.
             juego = (Image.open(cell.path)).resize((100, 100), Image.Resampling.LANCZOS)
             cell.image_tk = ImageTk.PhotoImage(juego)
 
+            ## Lo que se mostrará en la ventana:
             label = ttk.Label(root, image=cell.image_tk, text=cell.title, compound= tk.BOTTOM)
             label.grid(row=0,column=i)
             label.bind("<Button-1>",lambda event, celda = cell: self.on_button_clicked(cell))
