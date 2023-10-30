4aa1e05162b96f3aa47f291cd8e645fd99d52c75
Bl4nc018 <abr04work@gmail.com>
2023-10-30 17:46:37 +0100

    Completada tarea #2: Añadido el botón en activity_catalog, creada la clase DetailActivity y programado el botón para que inicie la siguiente actividad al hacer click en el.

--- sprint3android/My Catalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/My Catalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,13 +2,28 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CatalogActivity extends AppCompatActivity {
-
+    private Context context=this; // Recordamos la importancia de definir el contexto donde se ejecutan las apps para emplearlo más adelante.
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // Importante la creación de este método, que iniciará los widgets una vez se abra la app.
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog); // Establecemos la vista principal de una actividad basandonos en el .xml que creamos.
+        Button boton = findViewById(R.id.navego); // Nos aseguramos de encontrar el botón que recién creamos.
+        boton.setOnClickListener(new View.OnClickListener() { // Seteamos un controlador en el botón para comprobar cuando es clicado.
+            @Override
+            public void onClick(View view) { // Aqui, una vez se ejecute el click sobre el botón:
+                Intent myIntent = new Intent(context, DetailActivity.class); // Instanciamos un intent para que, tras clickar en el botón se ejecute
+                                                                             // la clase DetailActivity
+                context.startActivity(myIntent);
+            }
+
+        });
     }
 }
