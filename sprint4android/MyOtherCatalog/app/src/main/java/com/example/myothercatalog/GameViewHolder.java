b135a496c6582b99711c66f45c0eb0f723ebfe70
Bl4nc018 <abr04work@gmail.com>
2023-11-24 21:16:02 +0100

    Completada tarea #2: Añadida la clase DetailActivity y el botón en el layout del main el cual nos llevará a esta.

--- sprint4android/MyOtherCatalog/app/src/main/java/com/example/myothercatalog/GameViewHolder.java
+++ sprint4android/MyOtherCatalog/app/src/main/java/com/example/myothercatalog/GameViewHolder.java
@@ -1,7 +1,10 @@
 package com.example.myothercatalog;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,16 +15,34 @@ public class GameViewHolder extends RecyclerView.ViewHolder{
     // Aquí nos encontramos los elementos de la vista de la celda
     private TextView name;
     private ImageView game;
+    private Button button;
 
     // Este constructor recibe la vista de la celda e inicializa los atributos con los elementos de la misma.
     public GameViewHolder(@NonNull View ivi) {
         super(ivi);
         name = ivi.findViewById(R.id.text_view_game);
         game = ivi.findViewById(R.id.image_view_game);
+
+        // Ejercicio #2: Declaramos tanto la variable en private como en GameViewHolder, en donde
+        // añadiremos una linea para encontrar en la vista el botón.
+        button = ivi.findViewById(R.id.button);
     }
     // Y en este último método se muestran los datos en la vista de la celda:
     public void showData(GameData gameData) {
-        this.name.setText(gameData.getName()); // Establecemos el nombre del juego en el TextView
+        name.setText(gameData.getName()); // Establecemos el nombre del juego en el TextView
         Util.downloadBitmapToImageView(gameData.getImage_url(), this.game); // Empleamos una utilidad para descargar y establecer la imagen del juego en el ImageView.
+
+        // Ejercicio #2: Añadimos un OnClickListener para que al pulsar el botón obtenga el contexto,
+        // cree un nuevo Intent con este el cual nos lleve hasta DetailActivity.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+
+                Intent intent = new Intent(context, DetailActivity.class);
+
+                context.startActivity(intent);
+            }
+        });
     }
 }
